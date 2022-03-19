fb42d7968fb5330078d28f2f0aaba333e7292dac
Dvuglaf <63191330+Dvuglaf@users.noreply.github.com>
2022-03-19 15:20:43 +0400

    Update linear.cpp

--- linear.cpp
+++ linear.cpp
@@ -1,29 +1,11 @@
-﻿// linear.cpp: определяет точку входа для приложения.
-//
 
-#include "linear.h"
-#include "matrix.h"
-#include <iostream>
-#include "complex.h"
-#include <vector>
-#include "vector.h"
 #include "console.h"
-#include <unordered_map>
-#include <variant>
-#include <stack>
-#include <regex>
-#include <ctime>
 
 //user_enter - все, что ввел пользователь
 //function - название функции
 //expression - выражение после названия функции в скобках или весь ввод слева.
 
-int main()
-{
-	/*
-		TODO: 
-	
-	*/
+int main() {
 	console c;
 	c();
 	
